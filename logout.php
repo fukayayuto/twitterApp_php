f72ffbd03cdc81aec855d1a996a302f13070dc4b
fukayayuto <66364169+fukayayuto@users.noreply.github.com>
2020-10-09 14:27:41 +0900

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+$_SESSION = array();
+if(ini_set('session.use_cookies')){
+    $params = session_get_cookie_params();
+    setcookie(session_name() . '',time() -42000,$params['secure'],$params['httponly']);
+}
+
+session_destroy;
+
+setcookie('email','',time()-42000);
+
+header('Location:index.php');
+exit();
+
+?>
