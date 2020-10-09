d41d7410ae9241c5cd6ee890f998513274353453
fukayayuto <66364169+fukayayuto@users.noreply.github.com>
2020-10-09 14:28:51 +0900

    Create db_conect.php

---
+++ db_conect.php
@@ -0,0 +1,7 @@
+<?php
+try{
+    $db = new PDO('mysql:dbname=mini_bbs;host=localhost;port=3306;charset=utf8','root','root'); 
+}catch(PDOException $e){
+    echo 'DB接続エラー:' .$e-> getMessage();
+}
+?>
